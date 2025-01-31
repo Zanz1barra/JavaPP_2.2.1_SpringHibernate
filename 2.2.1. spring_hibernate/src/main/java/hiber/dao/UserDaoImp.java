64a67d16142b617aaca0ac2ed6cb7aa6b050ea71
Pavel Naboka <Pavmn96@yandex.ru>
2025-01-31 08:08:23 +0300

    Completing first point of problem
    Created MarkDown file `problem.md` with problem statement
    Refactored code to improve readability

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,6 +1,7 @@
 package hiber.dao;
 
 import hiber.model.User;
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
